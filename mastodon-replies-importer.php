00ced2521e88115f36b8efffa44233c250811d04
Donncha O Caoimh <5656673+donnchawp@users.noreply.github.com>
2024-12-15 15:33:38 +0000

    Remove the Domain Path until there are translations

--- mastodon-replies-importer.php
+++ mastodon-replies-importer.php
@@ -9,7 +9,6 @@
  * License: GPL v2 or later
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain: mastodon-replies-importer
- * Domain Path: /languages
  *
  * @package MastodonRepliesImporter
  */
